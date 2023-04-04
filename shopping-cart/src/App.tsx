70cf178520ffa0a14f0fd6e03868a92d0d1e1aa5
Elizabeth Mashilo <elizabethmashilo@gmail.com>
2023-04-04 14:17:21 +0200

    Fix error on StoreItem function - compiler not reading props

---
+++ shopping-cart/src/App.tsx
@@ -0,0 +1,29 @@
+import {Routes, Route} from "react-router-dom"
+import {Container} from 'react-bootstrap'
+import { useState } from 'react'
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import {NavBar}from "./components/NavBar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ShoppingCartProvider>
+    <NavBar/>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/store" element={<Store/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Container>
+    </ShoppingCartProvider>
+  )
+}
+
+export default App
